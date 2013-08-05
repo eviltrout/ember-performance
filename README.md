898a3fb510cd2f7df17dc923e6dfa3587696ff8a
Robin Ward <robin.ward@gmail.com>
2013-08-05 11:58:29 -0400

    First version: Can profile `Ember.Object.create`

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 ember-performance
 =================
 
-A suite of tests for EmberJS to help with performance
+The goal of this project is to create a comprehensive suite of tests to improve
+the performance of Ember.js.
+
+
+## Usage
+
+1. Clone the repo
+2. Open in a browser
+3. Click the button for the test you want to run.
+
+
+## Running against a new Ember.js Build
+
+Simply replace the `ember-master.js` file with the latest *production* build of EmberJS
+you want to test.
+
+1. Clone ember-performance to your computer.
+2. Clone the [Ember.js](https://github.com/emberjs/ember.js) project someone on your computer.
+3. In the Ember.js directory, execute a command like so: `rake clean && rake dist && cp dist/ember.prod.js /Users/robin/code/ember-performance/js/libs/ember-master.js` (you will need to replace `/Users/robin/code/ember-performance` with the appropriate path on your computer.)
+4. Reload in your browser and run the tests!
+
+
+## License
+
+MIT
---
+++ js/app.js
@@ -0,0 +1,90 @@
+Perf = Ember.Application.create();
+
+Ember.Handlebars.helper('time', function(value, options) {
+  if (typeof value === "undefined" || value === 0) { return new Handlebars.SafeString("&mdash;"); }
+
+  var rounded = Math.floor(value * 100) / 100;
+  return new Handlebars.SafeString(rounded + 'ms');
+});
+
+Perf.Result = Ember.Object.extend({
+  init: function() {
+    this.set('times', []);
+  },
+
+  geometricMean: function() {
+    var result = 1;
+    this.get('times').forEach(function (t) {
+      result *= t;
+    });
+    return Math.pow(result, 1 / this.get('times.length'));
+  }.property('times.@each'),
+
+  mean: function() {
+    var result = 0;
+    this.get('times').forEach(function (t) {
+      result += t;
+    });
+    return result / this.get('times.length');
+  }.property('times.@each'),
+
+  standardDeviation: function() {
+    var result = 0,
+         mean = this.get('mean');
+
+    this.get('times').forEach(function (t) {
+      result += Math.pow(t - mean, 2);
+    });
+    console.log(result);
+    return result / this.get('times.length');
+  }.property('mean'),
+
+  start: function() {
+    this.set('timeStart', new Date().getTime());
+  },
+
+  stop: function() {
+    this.get('times').pushObject(new Date().getTime() - this.get('timeStart'));
+  }
+});
+
+Perf.ApplicationController = Ember.ArrayController.extend({
+
+  clear: function() {
+    this.get('model').clear();
+  },
+
+  profObjectCreate: function() {
+    this.profile("Object.create()", 50, function() {
+      for (var i=0; i<10000; i++) {
+        var instance = Ember.Object.create({});
+      }
+    });
+  },
+
+  profileMethod: function(result, test, moreTries) {
+    result.start();
+    test();
+    result.stop();
+
+    if (moreTries === 1) {
+      this.pushObject(result);
+      this.set('profiling', false);
+    } else {
+      // We delay between each run to allow the browser to clean up and stuff.
+      Em.run.later(this, 'profileMethod', result, test, moreTries - 1, 10);
+    }
+  },
+
+  profile: function(name, testRuns, test) {
+    var self = this;
+
+    self.set('profiling', true);
+    var result = Perf.Result.create({name: name});
+
+    Em.run.next(function () {
+      self.profileMethod(result, test, testRuns);
+    });
+  }
+
+});
