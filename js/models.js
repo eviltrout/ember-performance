988fd695f83010aef324de7cf50adf81570c6322
Robin Ward <robin.ward@gmail.com>
2014-10-23 14:55:38 -0400

    Support for canary ember builds + Handlebars 2.0

--- js/models.js
+++ js/models.js
@@ -4,7 +4,7 @@ Perf.ProfilerDisplay = Ember.Object.extend({
   profiling: Em.computed.alias('currentProfiler.profiling'),
 
   init: function(){
-    var versions = ["1.5.1", "1.6.1", "1.7.0", "1.7.0a", "1.8.0"];
+    var versions = ["1.5.1", "1.6.1", "1.7.0", "1.7.0a", "1.8.0", "http://builds.emberjs.com/canary/ember.prod.js"];
     if(versions.indexOf(window.testVersion) === -1) {
       versions.push(window.testVersion);
     }
