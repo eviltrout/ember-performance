dadce19478297c68f7737df405d78e9cbb60e502
Robin Ward <robin.ward@gmail.com>
2014-11-26 14:51:29 -0500

    Unbound rendering test

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@
     {name: 'Ember.set', path: '/ember-set'},
     {name: 'Ember.Object.Create', path: '/object-create'},
     {name: 'Render List', path: '/render-list'},
+    {name: 'Render List (Unbound)', path: '/render-list-unbound'},
+    {name: 'Render Complex List', path: '/render-complex-list'}
   ];
 
   var HANDLEBARS_DEFAULT = "/ember/handlebars-v1.3.0.js";
