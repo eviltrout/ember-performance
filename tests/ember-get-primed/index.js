65fd0b4b2308888093aa9418ffcd2e8c96e5c58e
Stefan Penner <stefan.penner@gmail.com>
2014-12-21 23:48:23 -0500

    add ember-get primed

---
+++ tests/ember-get-primed/index.js
@@ -0,0 +1,26 @@
+/* global TestClient */
+MicroTestClient.run({
+  name: 'Ember.get',
+
+  setup: function() {
+    var obj = Ember.Object.create({
+      thingId: 1234,
+      person: Ember.Object.create({
+        name: 'Evil Trout',
+        pet: Ember.Object.create({
+          name: 'Rover'
+        })
+      })
+    });
+
+    obj.get('thingId');
+    obj.get('person.name');
+    obj.get('person.pet.name');
+  },
+
+  test: function() {
+    obj.get('thingId');
+    obj.get('person.name');
+    obj.get('person.pet.name');
+  }
+});
