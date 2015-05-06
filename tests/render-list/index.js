a2f6bf032304dd7a09bf9d4bcb886ba5bdac628d
Matthew Beale <matt.beale@madhatted.com>
2015-05-05 20:10:10 -0400

    Add some virtual views to the each

--- tests/render-list/index.js
+++ tests/render-list/index.js
@@ -11,9 +11,13 @@
           "<tr>" +
             "<td class='name'>{{p.name}}</td>" +
             "<td class='email'>{{p.email}}</td>" +
-            "<td class='company'>{{p.company}}</td>" +
-            "<td class='city'>{{p.city}}</td>" +
-            "<td class='url'><a {{bind-attr href=p.url}}>Link</a></td>" +
+            "{{#with p.company as company}}" +
+              "<td class='company'>{{p.company}}</td>" +
+            "{{/with}}" +
+            "{{#if p.city}}" +
+              "<td class='city'>{{p.city}}</td>" +
+              "<td class='url'><a {{bind-attr href=p.url}}>Link</a></td>" +
+            "{{/if}}" +
           "</tr>" +
         "{{/each}}" +
       "</tbody>" +
