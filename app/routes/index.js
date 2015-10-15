54670dc3d529f10c5cb4a0f897873679560fe382
Stefan Penner <stefan.penner@gmail.com>
2015-10-15 17:34:20 +0300

    load benchmarks automatically
    * introduce bench.json
    * bench.json must exists in benchmarks/**/
    * bench.json contains name + keywords (more likely later)
    * keywords are intended as labels, allowing us to add more UI to group / bulk work with specific types of tests

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,42 +1,7 @@
 import Ember from 'ember';
 import config from 'ember-performance/config/environment';
 
-const TEST_LIST = [ // TODO: Populate this automatically from the test definitions
-  { name: 'Baseline: Render List',     path: '/baseline-render-list'     },
-  { name: 'Baseline: Handlebars List', path: '/baseline-handlebars-list' },
-
-  { name: 'Ember.get',                 path: '/ember-get' },
-  { name: 'Ember.set',                 path: '/ember-set' },
-  { name: 'Ember.set (primed)',        path: '/ember-set/primed' },
-  { name: 'Ember.get (primed)',        path: '/ember-get/primed' },
-  { name: 'Ember.run',                 path: '/ember-run' },
-
-  // this test is broken in 1.11 beta
-  // { name: 'link-to get(\'active\')',   path: '/link-to/active' },
-
-  { name: 'link-to get(\'create\')',   path: '/link-to/create' },
-
-  { name: 'object-create/component',   path: '/object-create/component'   },
-  { name: 'object-create/baseline',    path: '/object-create/baseline'   },
-  { name: 'object-create/index',       path: '/object-create' },
-
-  // Basically, browisers (v8 included have mega slow defineProp ...)
-  // - https://code.google.com/p/v8/issues/detail?id=3649
-  // - there is a FF ticket I opened, but couldn't find it quickly :P
-  // - Jakob is working on it from the v8 side.
-  { name: 'object-create/without-non-enumerable-safety', path: '/object-create/without-non-enumerable-safety' },
-  { name: 'object-create/without-non-enumerable-safety-same-class', path: '/object-create/without-non-enumerable-safety-same-class' },
-
-  { name: 'Render List',               path: '/render-list'   },
-  { name: 'Render List (Unbound)',     path: '/render-list-unbound' },
-  { name: 'Render Complex List',       path: '/render-complex-list' },
-  { name: 'Render Complex List (HTML)',path: '/render-complex-html' },
-
-  { name: 'Render Simple Ember List',  path: '/render-simple-ember-list' },
-  { name: 'Render List with link-to',  path: '/render-list-with-link-to' },
-  { name: 'Render link-to',            path: '/render-link-to' }
-];
-
+const BENCHMARKS = config.BENCHMARKS;
 const EMBER_VERSIONS = [];
 
 config.LOCAL_EMBER_VERSIONS.forEach(version => {
@@ -75,7 +40,7 @@ export default Ember.Route.extend({
   model() {
     let session = window.TestSession.recover();
 
-    let tests = TEST_LIST.map(test => {
+    let tests = BENCHMARKS.map(test => {
       if (session) {
         test.isEnabled = session.isTestEnabled(test);
       } else {
