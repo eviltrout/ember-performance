5c7158d2f9c5eaaeba003d5c02bad33c9389b390
Jonathan Jackson <jonathan.jackson1@me.com>
2016-06-26 20:39:05 -0400

    Add Ember.String.classify benchmark

---
+++ benchmarks/string-classify/index.js
@@ -0,0 +1,36 @@
+(function(){
+  MicroTestClient.run({
+    name: 'String Classify',
+
+    setup: function() {
+      function createRandomWord(length) {
+        var consonants = 'bcdfghjklmnpqrstvwxyz',
+          vowels = 'aeiou',
+          rand = function(limit) {
+            return Math.floor(Math.random()*limit);
+          },
+          i, word='', length = parseInt(length,10),
+          consonants = consonants.split(''),
+          vowels = vowels.split('');
+          for (i=0;i<length/2;i++) {
+            var randConsonant = consonants[rand(consonants.length)],
+              randVowel = vowels[rand(vowels.length)];
+            word += (i===0) ? randConsonant.toUpperCase() : randConsonant;
+            word += i*2<length-1 ? randVowel : '';
+          }
+          return word;
+      }
+      var stringArray = [];
+      for (var i = 0; i < 1000; i++) {
+        stringArray.push(createRandomWord(5));
+      }
+    },
+
+    test: function(){
+      stringArray.forEach(function(string) {
+        Ember.String.classify(string)
+      });
+    }
+  });
+
+})();
