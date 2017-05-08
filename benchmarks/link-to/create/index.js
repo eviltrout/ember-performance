77f2155b702b9118ef543017c8062ee533f41025
Robin Ward <robin.ward@gmail.com>
2017-05-08 11:47:50 -0400

    Remove broken Link tests

--- benchmarks/link-to/create/index.js
+++
@@ -1,8 +0,0 @@
-/* global TestClient */
-MicroTestClient.run({
-  name: 'Ember.LinkView.create',
-  test: function() {
-    var LinkView = Ember.LinkView || Ember.LinkComponent;
-    LinkView.create();
-  }
-});
