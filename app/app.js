62a027d0da8e629b79b204e1a6d19b42f3d5b559
Robin Ward <robin.ward@gmail.com>
2021-05-11 13:48:17 -0400

    FIX: We don't have an app global anymore to get the version

--- app/app.js
+++ app/app.js
@@ -1,16 +1,23 @@
-import Ember from 'ember';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Ember from "ember";
+import Resolver from "./resolver";
+import loadInitializers from "ember-load-initializers";
+import config from "./config/environment";
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+export let version;
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+
+  init() {
+    this._super(...arguments);
+    version = this.version;
+  },
 });
 
 loadInitializers(App, config.modulePrefix);
--- app/components/benchmark-report.js
+++ app/components/benchmark-report.js
@@ -1,31 +1,32 @@
-import Ember from 'ember';
-import numeral from 'numeral';
+import Ember from "ember";
+import numeral from "numeral";
+import { version } from "ember-performance/app";
 
 const {
   computed,
-  computed: { equal, gt }
+  computed: { equal, gt },
 } = Ember;
 
 export default Ember.Component.extend({
   ajax: Ember.inject.service(),
-  mode: 'html',
-  isHtmlMode: equal('mode', 'html'),
-  isTextMode: equal('mode', 'text'),
-  canSubmitStats: gt('report.testGroupReports.length', 0),
-  showGraph: gt('report.testGroupReports.length', 1),
+  mode: "html",
+  isHtmlMode: equal("mode", "html"),
+  isTextMode: equal("mode", "text"),
+  canSubmitStats: gt("report.testGroupReports.length", 0),
+  showGraph: gt("report.testGroupReports.length", 1),
 
-  groupedTests: computed('report.testGroupReports.[]', function() {
+  groupedTests: computed("report.testGroupReports.[]", function () {
     var tests = {};
-    this.get('report.testGroupReports').forEach((testGroupReport) => {
+    this.get("report.testGroupReports").forEach((testGroupReport) => {
       testGroupReport.results.forEach((result) => {
         var test = tests[result.name] || {
           name: result.name,
           data: [],
-          chartData: [["Ember Version", "Time in ms (lower is better)"]]
+          chartData: [["Ember Version", "Time in ms (lower is better)"]],
         };
         test.data.push({
           emberVersion: testGroupReport.emberVersion,
-          result: result
+          result: result,
         });
         test.chartData.push([testGroupReport.emberVersion.name, result.mean]);
 
@@ -36,38 +37,40 @@ export default Ember.Component.extend({
     return tests;
   }),
 
-  asciiTable: computed('report.testGroupReports.[]', function() {
-    var result = 'User Agent: ' + navigator.userAgent + '\n';
+  asciiTable: computed("report.testGroupReports.[]", function () {
+    var result = "User Agent: " + navigator.userAgent + "\n";
 
-    var featureFlags = this.get('report.featureFlags');
+    var featureFlags = this.get("report.featureFlags");
     if (featureFlags && featureFlags.length) {
-      result += 'Feature Flags: ' + featureFlags.join(', ') + '\n';
+      result += "Feature Flags: " + featureFlags.join(", ") + "\n";
     }
-    result += '\n';
+    result += "\n";
 
-    var table = new window.AsciiTable('Ember Performance Suite - Results');
+    var table = new window.AsciiTable("Ember Performance Suite - Results");
 
-    table.setHeading('Name', 'Speed', 'Error', 'Samples', 'Mean');
+    table.setHeading("Name", "Speed", "Error", "Samples", "Mean");
 
-    this.get('report.testGroupReports').forEach(testGroupReport => {
-      table.addRow(' -- Ember ' + testGroupReport.emberVersion.name + ' -- ');
+    this.get("report.testGroupReports").forEach((testGroupReport) => {
+      table.addRow(" -- Ember " + testGroupReport.emberVersion.name + " -- ");
 
-      testGroupReport.results.forEach(result => {
-        table.addRow(result.name,
-                     numeral(result.hz).format('0,0.00') + ' / sec',
-                     '∓' + numeral(result.rme).format('0,0.00') + '%',
-                     numeral(result.samples).format(),
-                     numeral(result.mean).format('0,0.00') + ' ms');
+      testGroupReport.results.forEach((result) => {
+        table.addRow(
+          result.name,
+          numeral(result.hz).format("0,0.00") + " / sec",
+          "∓" + numeral(result.rme).format("0,0.00") + "%",
+          numeral(result.samples).format(),
+          numeral(result.mean).format("0,0.00") + " ms"
+        );
       });
     });
 
     return result + table.toString();
   }),
 
-  remoteReports: computed('report.testGroupReports.[]', function() {
-    var featureFlags = this.get('report.featureFlags');
+  remoteReports: computed("report.testGroupReports.[]", function () {
+    var featureFlags = this.get("report.featureFlags");
 
-    return this.get('report.testGroupReports').map(testGroupReport => {
+    return this.get("report.testGroupReports").map((testGroupReport) => {
       return {
         id: testGroupReport.id,
         emberUrl: testGroupReport.emberVersion.path,
@@ -75,35 +78,35 @@ export default Ember.Component.extend({
         emberVersion: testGroupReport.emberVersion.name,
         featureFlags: featureFlags,
         results: testGroupReport.results,
-        emberPerfVersion: window.EmberPerformance.version
+        emberPerfVersion: version,
       };
     });
   }),
 
   actions: {
     switchMode(mode) {
-      this.set('mode', mode);
+      this.set("mode", mode);
     },
 
     submitResults() {
-      var ajax = this.get('ajax');
+      var ajax = this.get("ajax");
 
       this.setProperties({
         sending: true,
-        error: false
+        error: false,
       });
 
-      var reports = this.get('remoteReports').map(remoteReport => {
-        return ajax.request('http://perflogger.eviltrout.com/api/results', {
-          type: 'POST',
-          data: { results: JSON.stringify(remoteReport) }
+      var reports = this.get("remoteReports").map((remoteReport) => {
+        return ajax.request("http://perflogger.eviltrout.com/api/results", {
+          type: "POST",
+          data: { results: JSON.stringify(remoteReport) },
         });
       });
 
-      Ember.RSVP.Promise.all(reports).
-        then(() => this.set('sent', true)).
-        catch(() => this.set('error', true)).
-        finally(() => this.set('sending', false));
+      Ember.RSVP.Promise.all(reports)
+        .then(() => this.set("sent", true))
+        .catch(() => this.set("error", true))
+        .finally(() => this.set("sending", false));
     },
-  }
+  },
 });
