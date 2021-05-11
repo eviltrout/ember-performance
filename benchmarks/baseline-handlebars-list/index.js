adbbd75b0afeb262ad787122247fae42ff673b75
Robin Ward <robin.ward@gmail.com>
2021-05-11 12:22:33 -0400

    Use HTTPS

--- benchmarks/baseline-handlebars-list/index.js
+++ benchmarks/baseline-handlebars-list/index.js
@@ -1,39 +1,41 @@
 /* global TestClient */
-(function() {
-
+(function () {
   var compiled;
 
   var template =
     "<table>" +
-      "<tbody>" +
-        "{{#each people}}" +
-          "<tr>" +
-            "<td class='name'>{{name}}</td>" +
-            "<td class='email'>{{email}}</td>" +
-            "<td class='company'>{{company}}</td>" +
-            "<td class='city'>{{city}}</td>" +
-            "<td class='url'><a href='{{url}}'>Link</a></td>" +
-          "</tr>" +
-        "{{/each}}" +
-      "</tbody>" +
+    "<tbody>" +
+    "{{#each people}}" +
+    "<tr>" +
+    "<td class='name'>{{name}}</td>" +
+    "<td class='email'>{{email}}</td>" +
+    "<td class='company'>{{company}}</td>" +
+    "<td class='city'>{{city}}</td>" +
+    "<td class='url'><a href='{{url}}'>Link</a></td>" +
+    "</tr>" +
+    "{{/each}}" +
+    "</tbody>" +
     "</table>";
 
   TestClient.run({
-    name: 'Baseline: Handlebars List',
+    name: "Baseline: Handlebars List",
 
-    setup: function() {
-      return $.getScript('http://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-v2.0.0.js').then(function() {
+    setup: function () {
+      return $.getScript(
+        "https://builds.handlebarsjs.com.s3.amazonaws.com/handlebars-v2.0.0.js"
+      ).then(function () {
         compiled = Handlebars.compile(template);
       });
     },
 
-    reset: function() {
-      document.getElementById('scratch').innerHTML = '';
+    reset: function () {
+      document.getElementById("scratch").innerHTML = "";
     },
 
-    test: function() {
-      document.getElementById('scratch').innerHTML = compiled({ people: TestClient.PEOPLE_JSON });
-    }
+    test: function () {
+      document.getElementById("scratch").innerHTML = compiled({
+        people: TestClient.PEOPLE_JSON,
+      });
+    },
   });
-
 })();
